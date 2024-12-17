df5080689491642c2eab2bbe881bf13228f6b5d1
Muhammed Talha Kavak <146962096+talhakvk@users.noreply.github.com>
2024-12-17 15:38:57 +0300

    CalismaSorulari

---
+++ CalismaSoruları/SoruCozumleri1/SoruCozumleri1/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+
+namespace SoruCozumleri1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program başlatılıyor...\n");
+
+            // Soru 1: Şirket Çalışan Yönetimi
+            // Soru1CalisanYonetimi.Run();
+
+            // Soru 2: Hayvanat Bahçesi Yönetim Sistemi
+            // Soru2HayvanatBahcesi.Run();
+
+            // Soru 3: Banka Hesap Yönetimi
+            // Soru3BankaHesapYonetimi.Run();
+
+            Console.WriteLine("\nProgram sonlandırılıyor...");
+            Console.ReadLine();
+        }
+    }
+
+    #region Soru 1: Şirket Çalışan Yönetimi
+    // Temel Calisan sınıfı
+    class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public decimal Maas { get; set; }
+        public string Pozisyon { get; set; }
+
+        // Virtual metod
+        public virtual void BilgiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}, Maaş: {Maas}, Pozisyon: {Pozisyon}");
+        }
+    }
+
+    // Yazilimci sınıfı
+    class Yazilimci : Calisan
+    {
+        public string YazilimDili { get; set; }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine($"Yazılım Dili: {YazilimDili}");
+        }
+    }
+
+    // Muhasebeci sınıfı
+    class Muhasebeci : Calisan
+    {
+        public string MuhasebeYazilimi { get; set; }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine($"Muhasebe Yazılımı: {MuhasebeYazilimi}");
+        }
+    }
+
+    // Soru 1 Çalıştırma sınıfı
+    class Soru1CalisanYonetimi
+    {
+        public static void Run()
+        {
+            Console.WriteLine("1. Çalışan Yönetimi\n");
+
+            Console.WriteLine("Çalışan türü seçiniz: (1- Yazilimci, 2- Muhasebeci)");
+            int secim = int.Parse(Console.ReadLine());
+
+            if (secim == 1)
+            {
+                Yazilimci yazilimci = new Yazilimci
+                {
+                    Ad = "Ali",
+                    Soyad = "Yılmaz",
+                    Maas = 15000,
+                    Pozisyon = "Kıdemli Yazılım Geliştirici",
+                    YazilimDili = "C#"
+                };
+                yazilimci.BilgiYazdir();
+            }
+            else if (secim == 2)
+            {
+                Muhasebeci muhasebeci = new Muhasebeci
+                {
+                    Ad = "Ayşe",
+                    Soyad = "Kara",
+                    Maas = 12000,
+                    Pozisyon = "Muhasebe Müdürü",
+                    MuhasebeYazilimi = "Logo"
+                };
+                muhasebeci.BilgiYazdir();
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz seçim!");
+            }
+        }
+    }
+    #endregion
+
+    #region Soru 2: Hayvanat Bahçesi Yönetimi
+    // Temel Hayvan sınıfı
+    class Hayvan
+    {
+        public string Ad { get; set; }
+        public string Tur { get; set; }
+        public int Yas { get; set; }
+
+        public virtual void SesCikar()
+        {
+            Console.WriteLine("Hayvan sesi çıkarıyor...");
+        }
+    }
+
+    // Memeli sınıfı
+    class Memeli : Hayvan
+    {
+        public string TuyRengi { get; set; }
+
+        public override void SesCikar()
+        {
+            Console.WriteLine($"{Ad} adlı memeli mırıldanıyor.");
+        }
+    }
+
+    // Kuş sınıfı
+    class Kus : Hayvan
+    {
+        public double KanatGenisligi { get; set; }
+
+        public override void SesCikar()
+        {
+            Console.WriteLine($"{Ad} adlı kuş ötüyor.");
+        }
+    }
+
+    class Soru2HayvanatBahcesi
+    {
+        public static void Run()
+        {
+            Console.WriteLine("2. Hayvanat Bahçesi Yönetimi\n");
+
+            Console.WriteLine("Hayvan türü seçiniz: (1- Memeli, 2- Kuş)");
+            int secim = int.Parse(Console.ReadLine());
+
+            if (secim == 1)
+            {
+                Memeli memeli = new Memeli
+                {
+                    Ad = "Aslan",
+                    Tur = "Memeli",
+                    Yas = 5,
+                    TuyRengi = "Altın Sarısı"
+                };
+                memeli.SesCikar();
+            }
+            else if (secim == 2)
+            {
+                Kus kus = new Kus
+                {
+                    Ad = "Kartal",
+                    Tur = "Kuş",
+                    Yas = 3,
+                    KanatGenisligi = 2.5
+                };
+                kus.SesCikar();
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz seçim!");
+            }
+        }
+    }
+    #endregion
+
+    #region Soru 3: Banka Hesap Yönetimi
+    // Temel Hesap sınıfı
+    class Hesap
+    {
+        public string HesapNumarasi { get; set; }
+        public decimal Bakiye { get; set; }
+        public string HesapSahibi { get; set; }
+
+        public virtual void ParaYatir(decimal miktar)
+        {
+            Bakiye += miktar;
+        }
+
+        public virtual void ParaCek(decimal miktar)
+        {
+            if (Bakiye >= miktar)
+            {
+                Bakiye -= miktar;
+            }
+        }
+
+        public virtual void BilgiYazdir()
+        {
+            Console.WriteLine($"Hesap Sahibi: {HesapSahibi}, Bakiye: {Bakiye}");
+        }
+    }
+
+    class VadesizHesap : Hesap
+    {
+        public decimal EkHesapLimiti { get; set; }
+    }
+
+    class VadeliHesap : Hesap
+    {
+        public int Vade { get; set; }
+        public decimal FaizOrani { get; set; }
+    }
+
+    class Soru3BankaHesapYonetimi
+    {
+        public static void Run()
+        {
+            Console.WriteLine("3. Banka Hesap Yönetimi\n");
+
+            Console.WriteLine("Hesap türü seçiniz: (1- Vadesiz Hesap, 2- Vadeli Hesap)");
+            int secim = int.Parse(Console.ReadLine());
+
+            if (secim == 1)
+            {
+                VadesizHesap vadesiz = new VadesizHesap
+                {
+                    HesapNumarasi = "12345",
+                    HesapSahibi = "Ahmet",
+                    Bakiye = 1000,
+                    EkHesapLimiti = 500
+                };
+                vadesiz.BilgiYazdir();
+            }
+            else if (secim == 2)
+            {
+                VadeliHesap vadeli = new VadeliHesap
+                {
+                    HesapNumarasi = "67890",
+                    HesapSahibi = "Mehmet",
+                    Bakiye = 5000,
+                    Vade = 12,
+                    FaizOrani = 0.05m
+                };
+                vadeli.BilgiYazdir();
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz seçim!");
+            }
+        }
+    }
+    #endregion
+}
---
+++ CalismaSoruları/SoruCozumleri2/SoruCozumleri2/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoruCozumleri2
+{
+    // 1. Görev: Banka Hesap Yönetim Sistemi
+    #region Banka Hesap Yönetimi
+    // Soyut Hesap Sınıfı
+    public abstract class Hesap
+    {
+        public string HesapNo { get; set; }
+        public decimal Bakiye { get; set; }
+
+        public Hesap(string hesapNo, decimal bakiye)
+        {
+            HesapNo = hesapNo;
+            Bakiye = bakiye;
+        }
+
+        public abstract void ParaYatir(decimal miktar);
+        public abstract void ParaCek(decimal miktar);
+    }
+
+    // IBankaHesabi Arayüzü
+    public interface IBankaHesabi
+    {
+        DateTime HesapAcilisTarihi { get; set; }
+        void HesapOzeti();
+    }
+
+    // BirikimHesabi Sınıfı
+    public class BirikimHesabi : Hesap, IBankaHesabi
+    {
+        public decimal FaizOrani { get; set; }
+        public DateTime HesapAcilisTarihi { get; set; }
+
+        public BirikimHesabi(string hesapNo, decimal bakiye, decimal faizOrani)
+            : base(hesapNo, bakiye)
+        {
+            FaizOrani = faizOrani;
+            HesapAcilisTarihi = DateTime.Now;
+        }
+
+        public override void ParaYatir(decimal miktar)
+        {
+            Bakiye += miktar + (miktar * FaizOrani / 100);
+        }
+
+        public override void ParaCek(decimal miktar)
+        {
+            if (Bakiye >= miktar)
+                Bakiye -= miktar;
+            else
+                Console.WriteLine("Yetersiz bakiye.");
+        }
+
+        public void HesapOzeti()
+        {
+            Console.WriteLine($"Birikim Hesabı: {HesapNo}, Bakiye: {Bakiye:C}, Faiz Oranı: {FaizOrani}%, Açılış Tarihi: {HesapAcilisTarihi}");
+        }
+    }
+
+    // VadesizHesap Sınıfı
+    public class VadesizHesap : Hesap, IBankaHesabi
+    {
+        public DateTime HesapAcilisTarihi { get; set; }
+        private decimal IslemUcreti = 2m;
+
+        public VadesizHesap(string hesapNo, decimal bakiye)
+            : base(hesapNo, bakiye)
+        {
+            HesapAcilisTarihi = DateTime.Now;
+        }
+
+        public override void ParaYatir(decimal miktar)
+        {
+            Bakiye += miktar;
+        }
+
+        public override void ParaCek(decimal miktar)
+        {
+            decimal toplamCekim = miktar + IslemUcreti;
+            if (Bakiye >= toplamCekim)
+                Bakiye -= toplamCekim;
+            else
+                Console.WriteLine("Yetersiz bakiye.");
+        }
+
+        public void HesapOzeti()
+        {
+            Console.WriteLine($"Vadesiz Hesap: {HesapNo}, Bakiye: {Bakiye:C}, Açılış Tarihi: {HesapAcilisTarihi}");
+        }
+    }
+    #endregion
+
+    // 2. Görev: Mağaza Yönetim Sistemi
+    #region Magaza Urun Yönetimi
+    // Soyut Ürün Sınıfı
+    public abstract class Urun
+    {
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+
+        public Urun(string ad, decimal fiyat)
+        {
+            Ad = ad;
+            Fiyat = fiyat;
+        }
+
+        public abstract decimal HesaplaOdeme();
+        public abstract void BilgiYazdir();
+    }
+
+    // Kitap Sınıfı
+    public class Kitap : Urun
+    {
+        public Kitap(string ad, decimal fiyat) : base(ad, fiyat) { }
+
+        public override decimal HesaplaOdeme()
+        {
+            return Fiyat * 1.10m; // %10 vergi
+        }
+
+        public override void BilgiYazdir()
+        {
+            Console.WriteLine($"Kitap: {Ad}, Fiyat: {Fiyat:C}, Ödenecek: {HesaplaOdeme():C}");
+        }
+    }
+
+    // Elektronik Sınıfı
+    public class Elektronik : Urun
+    {
+        public Elektronik(string ad, decimal fiyat) : base(ad, fiyat) { }
+
+        public override decimal HesaplaOdeme()
+        {
+            return Fiyat * 1.25m; // %25 vergi
+        }
+
+        public override void BilgiYazdir()
+        {
+            Console.WriteLine($"Elektronik: {Ad}, Fiyat: {Fiyat:C}, Ödenecek: {HesaplaOdeme():C}");
+        }
+    }
+    #endregion
+
+    // 3. Görev: Observer Design Pattern ile Yayıncı-Abone Sistemi
+    #region Observer Yayinci-Abone Sistemi
+    public interface IYayinci
+    {
+        void AboneEkle(IAbone abone);
+        void AboneCikar(IAbone abone);
+        void Bildir();
+    }
+
+    public interface IAbone
+    {
+        void BilgiAl(string mesaj);
+    }
+
+    public class Yayinci : IYayinci
+    {
+        private List<IAbone> aboneler = new List<IAbone>();
+        private string guncelleme;
+
+        public void AboneEkle(IAbone abone) => aboneler.Add(abone);
+        public void AboneCikar(IAbone abone) => aboneler.Remove(abone);
+
+        public void GuncellemeYap(string mesaj)
+        {
+            guncelleme = mesaj;
+            Bildir();
+        }
+
+        public void Bildir()
+        {
+            foreach (var abone in aboneler)
+                abone.BilgiAl(guncelleme);
+        }
+    }
+
+    public class Abone : IAbone
+    {
+        private string Ad { get; set; }
+
+        public Abone(string ad)
+        {
+            Ad = ad;
+        }
+
+        public void BilgiAl(string mesaj)
+        {
+            Console.WriteLine($"{Ad} adlı aboneye gelen güncelleme: {mesaj}");
+        }
+    }
+    #endregion
+
+    // Main Programı
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Banka Hesap Yönetimi Test
+            // Banka Hesap Yönetimi
+            BirikimHesabi birikimHesap = new BirikimHesabi("12345", 1000, 5);
+            VadesizHesap vadesizHesap = new VadesizHesap("67890", 500);
+
+            birikimHesap.ParaYatir(500);
+            vadesizHesap.ParaCek(100);
+
+            birikimHesap.HesapOzeti();
+            vadesizHesap.HesapOzeti();
+            #endregion
+
+            #region Mağaza Ürün Yönetimi Test
+            // Mağaza Ürün Yönetimi
+            List<Urun> urunler = new List<Urun>
+            {
+                new Kitap("C# Programlama", 50),
+                new Elektronik("Laptop", 3000)
+            };
+
+            foreach (var urun in urunler)
+            {
+                urun.BilgiYazdir();
+            }
+            #endregion
+
+            #region Observer Yayıncı-Abone Test
+            // Observer Design Pattern
+            Yayinci yayinci = new Yayinci();
+            Abone abone1 = new Abone("Talha");
+            Abone abone2 = new Abone("Ahmet");
+
+            yayinci.AboneEkle(abone1);
+            yayinci.AboneEkle(abone2);
+
+            yayinci.GuncellemeYap("Yeni içerik yüklendi!");
+            #endregion
+        }
+    }
+}
